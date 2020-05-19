e50835b594dcebcfa02fc382b92aa66a26732917
Norby <norbert.hambuer@gmail.com>
2020-05-19 09:44:20 +0300

    Paths corrections

--- index.php
+++ index.php
@@ -251,8 +251,7 @@ $topItems = $shop->getTopRatings();
 </footer>
 
 <!-- Bootstrap core JavaScript -->
-<script src="vendor/jquery/jquery.min.js"></script>
-<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
 <script>
